e7ce774cebb359a4d0a8f31487f3bef5d9c791c4
Ridho-Chaniago <ridho12c.rc@gmail.com>
2025-04-07 17:17:25 +0700

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/molecules/navbar';
 import Dashboard from './components/pages/Dashboard';
 import Portfolio from './components/pages/Portfolio';
